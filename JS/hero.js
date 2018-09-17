4605a94e1a2a55b3b5f75186f1e91a833a0f75d9
AlistairSimpsonDevelopment <asimpson.roomio@gmail.com>
2018-09-17 09:40:46 +0100

    more updates

--- JS/hero.js
+++ JS/hero.js
@@ -6,11 +6,19 @@ function fullscreen() {
     height: jQuery(window).height() });
 }
 
+function halfscreen() {
+  jQuery('#hero-2').css({
+    width: jQuery(window).width(),
+    height: jQuery(window).height()/1.8 });
+}
+
 fullscreen();
+halfscreen();
 
 // Run the function in case of window resize
 jQuery(window).resize(function () {
   fullscreen();
+  halfscreen();
 });
 
 });
