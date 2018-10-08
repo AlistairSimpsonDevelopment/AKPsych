366994d9d085677c547507aaa7b57cd734ec775c
AlistairSimpsonDevelopment <asimpson.roomio@gmail.com>
2018-10-08 16:05:52 +0100

    finishing touches

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 
 <!-- meta -->
-<title>Alex Kasozi Psychology</title>
+<title>Alexandra Kasozi Psychology</title>
 <link rel="alternate" hreflang="x-default" href="website name" />
 <link rel="canonical" href="" >
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,9 +20,8 @@
 
 <!-- css -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<link rel="stylesheet" href="Bootstrap/css/bootstrap.css" >
+<link rel="stylesheet" href="bootstrap/css/bootstrap.css" >
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-<link rel="shortcut icon" href="/favicon.ico" />
 <link rel="stylesheet" href="index.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous" >
 <link rel="stylesheet"
@@ -31,49 +30,49 @@
   <link rel="stylesheet"
   href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" >
 <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-<link rel="icon" href="Images/favicon.ico" type="image/x-icon" />
-<link rel="shortcut icon" href="Images/favicon.ico" type="image/x-icon" />
-<link rel="apple-touch-icon" sizes="57x57" href="Images/favicons//apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="Images/favicons//apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="Images/favicons//apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="Images/favicons//apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="Images/favicons//apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="Images/favicons//apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="Images/favicons//apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="Images/favicons//apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="Images/favicons//apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="Images/favicons/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="Images/favicons//favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="Images/favicons//favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="Images/favicons//favicon-16x16.png">
-<link rel="manifest" href="Images/favicons//manifest.json">
+<link rel="icon" href="images/favicons/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="images/favicons/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" sizes="57x57" href="images/favicons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="images/favicons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/favicons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="images/favicons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/favicons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="images/favicons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="images/favicons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="images/favicons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="images/favicons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="images/favicons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png">
+<link rel="manifest" href="images/favicons//manifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="images/favicons//ms-icon-144x144.png">
+<meta name="msapplication-TileImage" content="images/favicons/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 
 </head>
 
 <body>
 
-<?php include 'Modules/navbar.html'; ?>
+<?php include 'modules/navbar.html'; ?>
 
-<?php include 'Modules/hero.html'; ?>
+<?php include 'modules/hero.html'; ?>
 
-<?php include 'Modules/about_me.html'; ?>
+<?php include 'modules/about_me.html'; ?>
 
-<?php include 'Modules/approach.html'; ?>
+<?php include 'modules/approach.html'; ?>
 
-<?php include 'Modules/expertise.html'; ?>
+<?php include 'modules/expertise.html'; ?>
 
-<?php include 'Modules/quote.html'; ?>
+<?php include 'modules/quote.html'; ?>
 
-<?php include 'Modules/fees.html'; ?>
+<?php include 'modules/fees.html'; ?>
 
-<?php include 'Modules/contact.html'; ?>
+<?php include 'modules/contact.html'; ?>
 
-<?php include 'Modules/footer.html';?>
+<?php include 'modules/footer.html';?>
 
-<?php include 'Modules/modals.html';?>
+<?php include 'modules/modals.html';?>
 
 <!-- jquery -->
 <script
@@ -84,13 +83,13 @@
 
 <!-- bootstrap js -->
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"   ></script>
-<script src="Bootstrap/js/bootstrap.min.js"  ></script>
+<script src="bootstrap/js/bootstrap.min.js"  ></script>
 
 <!-- maps js -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCym_SIY9EI_7UNVL3-FkLLE-yfVQ5VWwY&&callback=initMap" async ></script>
 
 <!-- app js -->
-<script type="text/javascript" src="JS/app.js" ></script>
+<script type="text/javascript" src="js/app.js" ></script>
 
 <!-- animate on scroll -->
 <script src="https://unpkg.com/aos@next/dist/aos.js" ></script>
