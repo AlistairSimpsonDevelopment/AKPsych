381311f4a69a57a210d8d67e168bef3e6f7f27a4
AlistairSimpsonDevelopment <asimpson.roomio@gmail.com>
2018-10-11 22:18:24 +0100

    gtm, modals edit, and htaccess https rewrite

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 <meta name="keywords" content="Counselling, psychologist, London, Tooting, counsellor, psychology, psychotherapy, doctor, coaching, " />
 <meta name="robots" content="index, follow" />
 
+
 <!-- fonts -->
 <link href="https://fonts.googleapis.com/css?family=Gudea" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
@@ -50,10 +51,21 @@
 <meta name="msapplication-TileImage" content="images/favicons/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M3WCKSL');</script>
+<!-- End Google Tag Manager -->
+
 </head>
 
 <body>
-
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3WCKSL"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 <?php include 'modules/navbar.html'; ?>
 
 <?php include 'modules/hero.html'; ?>
